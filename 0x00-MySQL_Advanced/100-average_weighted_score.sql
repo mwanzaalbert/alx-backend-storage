5ae059e7320aeca80d424fe84451511141752b6e
mwanzaalbert <user.email>
2024-10-17 19:57:42 +0300

    100-average_weighted_score.sql updated

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,32 +1,34 @@
 DELIMITER //
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+-- create a stored procedure ComputeAverageWeightedScoreForUser that
+-- computes and store the average weighted score for a student.
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
+    DECLARE total_weighted_score INT DEFAULT 0;
     DECLARE total_weight INT DEFAULT 0;
-    DECLARE weighted_score FLOAT DEFAULT 0;
-    
-    -- Calculate the total weighted score and total weight
-    SELECT SUM(corrections.score * projects.weight) INTO weighted_score
-    FROM corrections
-    JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id;
 
-    SELECT SUM(projects.weight) INTO total_weight
-    FROM corrections
-    JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id;
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
 
-    -- Calculate average weighted score
-    IF total_weight > 0 THEN
-        SET weighted_score = weighted_score / total_weight;
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
     ELSE
-        SET weighted_score = 0; -- Avoid division by zero
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
     END IF;
-
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = weighted_score
-    WHERE id = user_id;
 END //
 
 DELIMITER ;
