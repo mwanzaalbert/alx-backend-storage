758eeed4406afe0da516cd642d5b666b42c4d648
mwanzaalbert <user.email>
2024-10-17 18:37:40 +0300

    7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Create the ComputeAverageScoreForUser stored procedure
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    -- Calculate the average score from the corrections table for the given user_id
+    SELECT AVG(score) INTO avg_score 
+    FROM corrections 
+    WHERE user_id = user_id;
+
+    -- Update the user's average_score in the users table
+    UPDATE users 
+    SET average_score = avg_score 
+    WHERE id = user_id;
+END;
+//
+
+DELIMITER ;
